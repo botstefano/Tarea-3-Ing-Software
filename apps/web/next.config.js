33b27ab223125f25653019931e68b61ada70e24f
botstefano <t1023300721@unitru.edu.pe>
2026-05-04 01:25:51 -0500

    Excluyo api/ del build de Next.js para GitHub Pages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  transpilePackages: [],
+  webpack: (config, { isServer }) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/api/**', '**/node_modules/**'],
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
